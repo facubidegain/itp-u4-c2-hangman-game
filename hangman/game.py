d463da411df306bcf3224664df3a5fe6babf7a3e
facubidegain <facundo@facundoferro.dev>
2019-05-07 22:56:20 +0000

    solucion

--- hangman/game.py
+++ hangman/game.py
@@ -1,23 +1,50 @@
 from .exceptions import *
+import random
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = ['python','java','php','swift']
 
 
 def _get_random_word(list_of_words):
-    pass
+    if len(list_of_words) == 0:
+        raise InvalidListOfWordsException()
+    else:
+        return random.choice(list_of_words)
 
 
 def _mask_word(word):
-    pass
+    if len(word)==0:
+        raise InvalidWordException()
+    return '*'*len(word)
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    ret = masked_word
+    if len(answer_word)==0 or len(masked_word)==0 or len(answer_word) != len(masked_word):
+        raise InvalidWordException()
+    if len(character)>1:
+        raise InvalidGuessedLetterException()
+    for index,value in enumerate(answer_word):
+        if character.lower() == value.lower():
+            ret = ret[:index] + character.lower() + ret[index+1:]
+    return ret
+    
 
 
 def guess_letter(game, letter):
-    pass
+    if game['remaining_misses'] == 0 or game['masked_word'] == game['answer_word']:
+        raise GameFinishedException()
+    res = _uncover_word(game['answer_word'],game['masked_word'],letter)
+    game['previous_guesses'].append(letter.lower())
+    if res == game['masked_word']:
+        game['remaining_misses'] -= 1
+    else:
+        game['masked_word'] = res
+        if game['masked_word'] == game['answer_word']:
+            raise GameWonException()
+    if game['remaining_misses'] == 0:
+        raise GameLostException()
+    return game
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
